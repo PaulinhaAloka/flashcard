06de047098e8797cee25ce26449f9075faba0ed7
UM ELEFAANTE INCOMODA MUITA GENTE <159184945+PaulinhaAloka@users.noreply.github.com>
2024-10-01 16:40:14 -0300

    1/10

---
+++ assets/pergunta.js
@@ -0,0 +1,18 @@
+criaCartao(
+'Programaçao',
+'O que é Python',
+'O Python é uma linguagem de programaçao',
+)
+
+criaCartao(
+'Geografia',
+'Qual é a capital da França?',
+'A capital da França é Paris'
+
+)
+
+criaCartao(
+'Programaçao',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa'
+)
